9baeb6c2fea4844746d071aa844cfda2ab3279fe
ehaddad2 <ehaddad2@u.rochester.edu>
2023-06-10 22:41:15 -0400

    Initial Commit

---
+++ Archgem/ArchgemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchgemApp.swift
+//  Archgem
+//
+//  Created by Elias Haddad on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ArchgemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
